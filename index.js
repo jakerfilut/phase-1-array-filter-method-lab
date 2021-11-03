19bbeb940ebdff7a38028af7e9325c1c1f2f0cc3
Jake <jakerfilut@gmail.com>
2021-11-03 09:36:41 -0600

    commit tree af626f3f5df5ca77809efafea9bd4369bef72033

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+
+function findMatching(drivers, name){
+    const result = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(drivers, letters){
+    const result = drivers.filter(driver => driver.startsWith(letters));
+    return result;
+}
+
+function matchName(drivers, string){
+    const results = drivers.filter(driver => driver.name === string);
+    return results;
+}
